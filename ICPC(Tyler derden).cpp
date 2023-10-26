b1713e941f951a17033fd2475ec414694f6607db
AlexHunter15 <134641085+AlexHunter15@users.noreply.github.com>
2023-10-26 19:25:39 +0300

    Add files via upload

---
+++ ICPC(Tyler derden).cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+    int p, q, a;
+    std::cin >> p >> q >> a;
+
+    int C = a / (1 + q + p * q);
+
+    int S = q * C;
+    int G = p * S;
+
+    std::cout << G << " " << S << " " << C;
+
+    return 0;
+}
